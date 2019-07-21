7a569ac4a06886fcde943abd94150114c8ab2f4a
kouui <lelouchkako@gmail.com>
2019-07-21 16:12:45 +0900

    modified : O_V_Ve_like.txt; completed : Grotrian.py 1. stage in O_V_Be_like.txt : - --> 3 2. Completed : Grotrian.py; with the following     - Function prepare_dict() {only for plotting grotrian diagram}     - Function line_with_text() {plotting a line with text on it}     - Class Grotrian() {class to generate grotrian diagram} 3. usage of Class Grotrian.Grotrian is shown in dubug_Grotrian.py 4. generate grotrian diagram for C III and O V

--- debug/debug_Grotrian.py
+++ debug/debug_Grotrian.py
@@ -7,9 +7,52 @@ if __name__ == "__main__":
 
     from src import AtomCls
     from src.visual import Grotrian
+    #-------------------------------------------------------------------------
+    # C III Be like
+    #-------------------------------------------------------------------------
+    ##filename = "/Users/liu/kouui/workspace/spectra/atom/C_III_Be_like.txt"
+    ##atom = AtomCls.Atom(filename)
+    ##gro = Grotrian.Grotrian(_atom=atom, _conf_duplicate="1s2.")
+    ##gro.make_fig(_figsize=(10,6))
+    ##
+    ##line_plot = (
+    ##    (0, 4, "977", 0.3, 0.5),
+    ##    (0, 2, "1909", 0.7, 0.1),
+    ##    (4, 9, "1247", 0.3, 0.5),
+    ##    (4, 8, "2297", 0.8, 0.5),
+    ##    (2, 5, "1175.99", 0.1, 0.1),
+    ##    (1, 6, "1175.26", 0.25, 0.25),
+    ##    (2, 6, "1175.59", 0.40, 0.40),
+    ##    (3, 6, "1176.38", 0.55, 0.55),
+    ##    (2, 7, "1174.93", 0.70, 0.70),
+    ##    (3, 7, "1175.71", 0.85, 0.85),
+    ##)
+    #-------------------------------------------------------------------------
 
-    atom = AtomCls.Atom("/Users/liu/kouui/workspace/spectra/atom/C_III_Be_like.txt")
-    gro = Grotrian.Grotrian(_atom=atom)
-    gro.make_fig(_figsize=(10,6))
-    gro.show_fig()
-    #gro.save_fig("./test.png")
+    #-------------------------------------------------------------------------
+    # O V Be like
+    #-------------------------------------------------------------------------
+    filename = "/Users/liu/kouui/workspace/spectra/atom/O_V_Be_like.txt"
+    atom = AtomCls.Atom(filename)
+    gro = Grotrian.Grotrian(_atom=atom, _conf_duplicate="1s2.")
+    gro.make_fig(_figsize=(10,6), _f=50)
+    
+    line_plot = (
+        (0, 4, "629.7", 0.3, 0.5),
+        (0, 2, "1218.3", 0.7, 0.1),
+        (4, 9, "774.5", 0.3, 0.5),
+        (4, 8, "1371.3", 0.8, 0.5),
+        (2, 5, "761.1", 0.1, 0.1),
+        (1, 6, "759.4", 0.25, 0.25),
+        (2, 6, "760.2", 0.40, 0.40),
+        (3, 6, "762.0", 0.55, 0.55),
+        (2, 7, "758.7", 0.70, 0.70),
+        (3, 7, "760.4", 0.85, 0.85),
+    )
+    #-------------------------------------------------------------------------
+    for i, j, wl, _r1, _r2 in line_plot:
+        _cfj1 = atom.Level_info_table[i]
+        _cfj2 = atom.Level_info_table[j]
+        gro.connect_line(_cfj1=_cfj1, _cfj2=_cfj2, _r1=_r1, _r2=_r2, _c="black", _text=wl, _tsize=7, _r=0.4)
+    #gro.show_fig()
+    gro.save_fig("../grotrian_diagram/" + filename.split('/')[-1].replace(".txt",".png"))
--- src/visual/Grotrian.py
+++ src/visual/Grotrian.py
@@ -72,7 +72,7 @@ def prepare_dict(_atom, _conf_duplicate):
     #-------------------------------------------------------------------------
     return _singlet, _multiplet, _Lset
 
-def line_with_text(_ax, _lp, _rp, _text, _tsize, _r):
+def line_with_text(_ax, _lp, _rp, _text, _tsize, _r, _tangle=0, _lcolor="black", _lwidth=0.7, _lstyle='-', _tcolor="black"):
     r"""
     Parameters
     ----------
@@ -95,6 +95,21 @@ def line_with_text(_ax, _lp, _rp, _text, _tsize, _r):
     _r : float, in the range of [0 : 1]
         relative position along the line starting from left point
 
+    _tangle : int or float
+        angle to rotate the text, default : 0
+
+    _lcolor : str
+        color of the line, default : "black"
+
+    _lwidth : float
+        line width, default : 0.7
+
+    _lstyle : str
+        line style, default : '-'
+
+    _tcolor : str
+        color of the text, default : "black"
+
     Returns
     -------
 
@@ -106,20 +121,30 @@ def line_with_text(_ax, _lp, _rp, _text, _tsize, _r):
     """
     # {'color': 'black', 'fontsize': 6, 'ha': 'center', 'va': 'center',
     #    'bbox': dict(boxstyle="round", fc="white", ec="white", pad=0.2)}
-    _line_obj, = _ax.plot([_lp[0], _rp[0]], [_lp[1], _rp[1]], "-", linewidth=0.7)
-    _angle = math.atan( (_rp[1]-_lp[1]) / (_rp[0]-_lp[0]) )
-    _angle = math.degrees( _angle )
+    _line_obj, = _ax.plot([_lp[0], _rp[0]], [_lp[1], _rp[1]], linestyle=_lstyle, linewidth=_lwidth, color=_lcolor)
     _tx = _lp[0] + (_rp[0]-_lp[0]) * _r
     _ty = _lp[1] + (_rp[1]-_lp[1]) * _r
-    _text_obj = _ax.text( _tx, _ty, _text, {'color': 'black', 'fontsize': _tsize, 'ha': 'center', 'va': 'center',
+    _text_obj = _ax.text( _tx, _ty, _text, {'color': _tcolor, 'fontsize': _tsize,
+                        'ha': 'center', 'va': 'center','rotation':_tangle,
                         'bbox': dict(boxstyle="round", fc="white", ec="white", pad=0.2)} )
 
     return _line_obj, _text_obj
 
 class Grotrian:
 
-    def __init__(self, _atom, _conf_duplicate="1s2."):
+    def __init__(self, _atom, _conf_duplicate):
+        r"""
 
+        Parameters
+        -----------
+
+        _atom : AtomCls.Atom
+            object of the atomic model
+
+        _conf_duplicate : str
+            common configuration string of the inner shell
+
+        """
         self.atom = _atom
 
         #---------------------------------------------------------------------
@@ -138,7 +163,20 @@ class Grotrian:
         #---------------------------------------------------------------------
 
     def make_fig(self, _figsize=(6,8), _dpi=120, _f=200):
-        # _f : enlarge factor insdie fine-structure
+        r"""
+
+        Parameters
+        ----------
+
+        _figsize : tuple of int
+            size of the figure, default : (6,8)
+
+        _dpi : int
+            "dot per inch" (resolution) of the figure, default : 120
+
+        _f : int
+            enlarge factor to show fine structure explicitly, default : 200
+        """
 
         Lset = self.Lset
         singlet = self.singlet
@@ -184,7 +222,7 @@ class Grotrian:
         #---------------------------------------------------------------------
         # plotting multiplet
         #---------------------------------------------------------------------
-        _hw = 0.6                       # multiplet need wider
+        _hw = 0.6                    # multiplet need wider
         _sf = 0.1                    # space for connecting fine structure
         for k0, v0 in multiplet.items():
             # compute mean term energy
@@ -207,11 +245,11 @@ class Grotrian:
                 pos_level[ (v1[2], k0[1], k1) ]["ys"] = ys_level
                 # connect fine structure
                 plt.plot([xs_level[1],xs_level[1]+_sf], [y_pos,y_mean], "--k", linewidth=0.5)
-                # plot text of term
-                x_pos_text = xs_level[1]+_sf + _st
-                plt.text(x_pos_text, y_mean, "{} {}".format(k0[0],k0[1]), fontsize=_textsize, color="k")
                 # plot text of J
                 plt.text(xs_level[0]-2*_st, y_pos, k1, fontsize=_Jsize, color="k")
+            # plot text of term
+            x_pos_text = xs_level[1]+_sf + _st
+            plt.text(x_pos_text, y_mean, "{} {}".format(k0[0],k0[1]), fontsize=_textsize, color="k")
         #---------------------------------------------------------------------
 
         #---------------------------------------------------------------------
@@ -236,7 +274,25 @@ class Grotrian:
         plt.xlim(-1, xtick2[-1]+2)
         #---------------------------------------------------------------------
 
+        ylim = self.fig.gca().get_ylim()
+        xlim = self.fig.gca().get_xlim()
+        # ylim_max - ylim_min
+        self.yr = ylim[1] - ylim[0]
+        # xlim_max - xlim_min
+        self.xr = xlim[1] - xlim[0]
+
     def save_fig(self, _filename, _dpi=120):
+        r"""
+
+        Parameters
+        ----------
+
+        _filename : str
+            filename (including path) to save your figure
+
+        _dpi : int
+            "dot per inch" (resolution) to save figure, default : 120s
+        """
 
         self.fig.savefig(_filename, dpi=_dpi)
 
@@ -244,7 +300,7 @@ class Grotrian:
 
         plt.show()
 
-    def connect_line(self, _cfj1, _cfj2, _r1, _r2, _c):
+    def connect_line(self, _cfj1, _cfj2, _r1, _r2, _c, _text, _tsize=7, _r=0.4, _lcolor="black", _lwidth=0.7, _lstyle='-', _tcolor="black"):
         r"""
 
         Parameters
@@ -257,8 +313,53 @@ class Grotrian:
             to specify level 2
 
         _r1 : float, in the range of [0 : 1]
+            relative position along the line of level 1 starting from left point
 
-            relative position along the line starting from left point
+        _r2 : float, in the range of [0 : 1]
+            relative position along the line of level 2 starting from left point
 
+        _c : str
+            color of the line you're going to draw
+
+        _text : str
+            content of text
+
+        _tsize : int
+            fontsize of text, default : 7
+
+        _r : float, in the range of [0 : 1]
+            relative position along the line starting from left point, default : 0.4
+
+        _lcolor : str
+            color of the line, default : "black"
+
+        _lwidth : float
+            line width, default : 0.7
+
+        _lstyle : str
+            line style, default : '-'
+
+        _tcolor : str
+            color of the text, default : "black"
         """
-        pass
+
+        pos_lvl1 = self.pos_level[_cfj1]
+        pos_lvl2 = self.pos_level[_cfj2]
+
+        # initialize left/right point of the line
+        _lp = pos_lvl1["xs"][0] + (pos_lvl1["xs"][1] - pos_lvl1["xs"][0])*_r1 , pos_lvl1["ys"][0]
+        _rp = pos_lvl2["xs"][0] + (pos_lvl2["xs"][1] - pos_lvl2["xs"][0])*_r2 , pos_lvl2["ys"][0]
+        # if the initialization is not correct, swap them
+        if _lp[0] > _rp[0] :
+            _lp, _rp = _rp, _lp
+
+        # compute the angle to rotate text
+        _w, _h = self.fig.get_size_inches()
+        _dx = (_rp[0]-_lp[0]) / self.xr * _w
+        _dy = (_rp[1]-_lp[1]) / self.yr * _h
+        _tangle = math.atan2( _dy, _dx )
+        _tangle = math.degrees( _tangle )
+        # plot line and text
+        _ax = self.fig.gca()
+        _line_obj, _text_obj = line_with_text(_ax=_ax, _lp=_lp, _rp=_rp, _text=_text, _tsize=_tsize, _r=0.5,
+                            _tangle=_tangle, _lcolor=_lcolor, _lwidth=_lwidth, _lstyle=_lstyle, _tcolor=_tcolor)
